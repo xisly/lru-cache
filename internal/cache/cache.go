55048caf6f48e4d24885659b1ebd649528b3fb6e
Ilya Kiselewski <ilyakiselewski@Ilyas-MacBook-Air.local>
2024-07-21 01:06:43 +0300

    implemented LRU interface with lazy ttl

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -31,22 +31,48 @@ type cache struct {
   cache map[string]*node
   left *node
   right *node
-  mu sync.RWMutex
+  mu sync.Mutex
 }
 
 func New(capacity int) ILRUCache {
-  return cache{}
+  ret := cache{
+    capacity: capacity
+    cache: make(map[string]node,capacity)
+    left: &node{key:"", value:""}
+    right: &node{key:"", value: ""}
+  }
+  ret.left.next, ret.right.prev = ret.right, ret.left
+  return ret
 }
 
-func (c *cache)Put(ctx context.Context, key string, value interface{} ttl time.Duration) error{
+func (c *cache) Put(ctx context.Context, key string, value interface{} ttl time.Duration) error{
   return nil
 }
 
-func (c *cache) Get(ctx context.Context, key string) (value interface{}, expiresAt time.Time)
+func (c *cache) insert(nd *node) {
+  prev, nxt := this.right.prev, this.right
+  nxt.prev = nd
+  prev.next = nxt.prev
+  node.next, node.prev = nxt, prev
+}
+
+func (this *LRUCache) remove(nd *node) {
+    prev, nxt := nd.prev, nd.next
+    prev.next, nxt.prev = nxt, prev
+}
+
+func (c *cache) Get(ctx context.Context, key string) (value interface{}, expiresAt time.Time, err error) {
+   if _, ok := this.cache[key]; ok {
+        this.Remove(this.cache[key])
+        this.Insert(this.cache[key])
+        return this.cache[key].val
+    }
+    return nil, time.Now(), errors.New()
+}
 
 func (c *cache) GetAll(ctx context.Context) (keys []string, values []interface{}, err error)
 
 func (c *cache) Evict(ctx context.Context, key string) (value interface{}, err error)
 
-func (c *cache) EvictAll(ctx context.Context) error
+func (c *cache) EvictAll(ctx context.Context) error {} 
 
