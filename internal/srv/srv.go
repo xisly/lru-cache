1152c16f89f11d53d79d082615f4872230b1827d
Ilya Kiselewski <ilyakiselewski@Ilyas-MacBook-Air.local>
2024-07-22 23:40:00 +0300

    added graceful shutdown logic

--- internal/srv/srv.go
+++ internal/srv/srv.go
@@ -2,33 +2,52 @@ package srv
 
 import (
 	"context"
+	"errors"
+	"log/slog"
 	"lru-cache/internal/cache"
+	"lru-cache/pkg/loghandler"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type Server struct {
 	storage cache.ILRUCache
 	router  chi.Router
 	cfg     Config
+	logger  *slog.Logger
 }
 
 type Config struct {
-	HostPort   string
-	Capacity   int
-	DefaultTTL time.Duration
+	HostPort   string        `env:"SERVER_HOST_PORT" envDefault:"localhost:8080"`
+	CacheSize  int           `env:"CACHE_SIZE" envDefault:"10"`
+	DefaultTTL time.Duration `env:"DEFAULT_CACHE_TTL" envDefault:"1m"`
+	LogLevel   string        `env:"LOG_LEVEL" envDefault:"DEBUG"`
 }
 
-func New(cfg Config) Server {
-	storage := cache.New(cfg.Capacity)
+func New(cfg Config) (*Server, error) {
+	levelhandler, err := loghandler.New(cfg.LogLevel, slog.NewTextHandler(os.Stdout, nil))
+	if err != nil {
+		return nil, err
+	}
+
+	logger := slog.New(levelhandler)
+
+	storage := cache.New(cfg.CacheSize)
+
 	router := chi.NewRouter()
 
-	return Server{storage: storage, router: router, cfg: cfg}
+	return &Server{storage: storage, router: router, cfg: cfg, logger: logger}, nil
 }
 
 func (s *Server) Run(ctx context.Context) error {
+
+	s.router.Use(s.loggingMiddleware, middleware.Recoverer)
 
 	s.router.Route("/api/lru", func(r chi.Router) {
 		r.Post("/", s.putKey)
@@ -38,10 +57,33 @@ func (s *Server) Run(ctx context.Context) error {
 		r.Delete("/", s.evictAllKeys)
 	})
 
-	err := http.ListenAndServe(s.cfg.HostPort, s.router)
-	if err != nil {
-		return err
+	server := http.Server{
+		Addr: s.cfg.HostPort,
+		Handler: s.router,
 	}
+
+	go func() {
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			s.logger.Error("HTTP server error", slog.Any("error", err))
+			os.Exit(1)
+		}
+		s.logger.Info("Stopped serving new connections.")
+	}()
+	
+	s.logger.Info("Running server")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+	
+	shutdownCtx, shutdownRelease := context.WithTimeout(ctx, 10*time.Second)
+	defer shutdownRelease()
+	
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		s.logger.Error("HTTP shutdown error", slog.Any("error",err))
+		os.Exit(2)
+	}
+	s.logger.Info("Graceful shutdown complete.")
 
 	return nil
 }
